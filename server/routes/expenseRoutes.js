95804ca198838f74aa628910b554652f1691a253
N1298275 <rahul.mestry2024@my.ntu.ac.uk>
2025-08-10 15:25:21 +0100

    delete old files

--- server/routes/expenseRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middlewares/auth');
-const { listExpenses, createExpense } = require('../controllers/expenseController');
-
-router.get('/', auth, listExpenses);
-router.post('/', auth, createExpense);
-
-module.exports = router;
--- server/routes/receiptRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { upload, uploadReceipt, processReceipt } = require('../controllers/receiptController');
-const auth = require('../middlewares/auth');
-
-router.post('/', auth, upload.single('receipt'), uploadReceipt);
-router.post('/:id/process', auth, processReceipt);
-
-module.exports = router;
